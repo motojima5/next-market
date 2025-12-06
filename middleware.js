6789741a1069036f5380a0a27d8b3b2887d43352
Motojima <r.motojima5@gmail.com>
2025-12-06 17:35:02 +0900

    アイテムデータの保存

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImV4cCI6MTc2NTAzOTQzM30.5kHf1a_Dg84KKCBFmb11Jzt8MXw-Ci3_DHR2Fl-Sz64";
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImV4cCI6MTc2NTA5NTM1NH0.-WxLG33A9Sg8L_yBSL8IX2jjSMZZyOea4heqQmATmgQ";
   // const token = await request.headers.get("Authorization")?.split(" ")[1];
   if (!token) {
     return NextResponse.json({ message: "トークンがありません" });
