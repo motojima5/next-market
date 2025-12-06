06afb2ab8626071dd2ac28ef7f07595fa767d9fb
Motojima <r.motojima5@gmail.com>
2025-12-06 19:16:38 +0900

    すべてのアイテムページを読み取るページ

--- app/page.js
+++ app/page.js
@@ -1,8 +1,34 @@
-const ReadAllItems = () => {
+import Image from "next/image";
+import Link from "next/link";
+
+const getAllItems = async () => {
+  const response = await fetch("http://localhost:3000/api/item/readall", {
+    cache: "no-store",
+  });
+  const jsonData = await response.json();
+  const allItems = jsonData.allItems;
+  return allItems;
+};
+
+const ReadAllItems = async () => {
+  const allItems = await getAllItems();
+
   return (
     <div>
-      <h1>こんにちは</h1>
-      <h3>さようなら</h3>
+      {allItems.map((item) => (
+        <Link href="" key={item._id}>
+          <Image
+            src={item.image}
+            width={750}
+            height={500}
+            alt="item-image"
+            priority
+          />
+          <h2>¥{item.price}</h2>
+          <h3>{item.title}</h3>
+          <p>{item.description.substring(0, 80)}</p>
+        </Link>
+      ))}
     </div>
   );
 };
