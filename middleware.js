e2a0110bfc33bd460651a30f4b9d01653c283857
Motojima <r.motojima5@gmail.com>
2025-12-06 22:23:12 +0900

    アイテムデータを作成するページ

--- middleware.js
+++ middleware.js
@@ -2,8 +2,7 @@ import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImV4cCI6MTc2NTA5NTM1NH0.-WxLG33A9Sg8L_yBSL8IX2jjSMZZyOea4heqQmATmgQ";
-  // const token = await request.headers.get("Authorization")?.split(" ")[1];
+  const token = await request.headers.get("Authorization")?.split(" ")[1];
   if (!token) {
     return NextResponse.json({ message: "トークンがありません" });
   }
